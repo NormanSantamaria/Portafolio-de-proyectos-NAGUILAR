436f1276bf48d7329780c76e69eb4478ee969621
Norman <nsantamaria_@outlook.es>
2024-11-22 19:47:58 -0600

    Añadir archivo fundamentos.js del dia 1

---
+++ fundamentos.js
@@ -0,0 +1,15 @@
+// Declaración de variables
+let nombre = "Emilio";
+let edad = 19;
+
+// Condicional
+if (edad >= 18) {
+  console.log(`${nombre} es mayor de edad.`);
+} else {
+  console.log(`${nombre} es menor de edad.`);
+}
+
+// Ciclo for
+for (let i = 0; i < 5; i++) {
+  console.log(`Iteración: ${i}`);
+}
